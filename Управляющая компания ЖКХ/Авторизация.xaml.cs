0e2f7ff4bbf63fc3b96727db43075454cc551bc1
ЗуеваЕЭ <zuevae778@gmail.com>
2022-12-15 13:19:22 +0300

    Код

--- Управляющая компания ЖКХ/Авторизация.xaml.cs
+++ Управляющая компания ЖКХ/Авторизация.xaml.cs
@@ -20,9 +20,48 @@ namespace Управляющая_компания_ЖКХ
     /// </summary>
     public partial class Авторизация : Page
     {
+        public string log;
+        public string par;
+
         public Авторизация()
         {
             InitializeComponent();
+        }
+
+
+        public DataTable Select(string selectSQL) // функция подключения к базе данных и обработка запросов
+        {
+            DataTable dataTable = new DataTable("dataBase");                // создаём таблицу в приложении
+                                                                            // подключаемся к базе данных
+            SqlConnection sqlConnection = new SqlConnection("Data Source=ngknn.ru;Initial Catalog=43П_ЖКХ;Persist Security Info=True;User ID=33П;Password=12357");
+            sqlConnection.Open();                                           // открываем базу данных
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();          // создаём команду
+            sqlCommand.CommandText = selectSQL;                             // присваиваем команде текст
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand); // создаём обработчик
+            sqlDataAdapter.Fill(dataTable);                                 // возращаем таблицу с результатом
+            sqlConnection.Close();
+            return dataTable;
+        }
+        private void Vxod_Click_Open(object sender, RoutedEventArgs e)
+        {
+            if (Log.Text.Length > 0) // проверяем введён ли логин     
+            {
+                if (Pas.Password.Length > 0) // проверяем введён ли пароль         
+                {             // ищем в базе данных пользователя с такими данными         
+                    DataTable dt_user = Select("SELECT * FROM [dbo].[Авторизация] WHERE [[Log]] = '" + Log.Text + "' AND [Pas] = '" + Pas.Password + "'");
+                    if (dt_user.Rows.Count > 0) // если такая запись существует       
+                    {
+                        MessageBox.Show("Пользователь авторизовался"); // говорим, что авторизовался 
+                        MainWindow mainWindow = new MainWindow();
+                        mainWindow.Show();
+                        Close();
+                    }
+                    else MessageBox.Show("Пользователя не найден"); // выводим ошибку  
+                }
+                else MessageBox.Show("Введите пароль"); // выводим ошибку    
+            }
+            else MessageBox.Show("Введите логин"); // выводим ошибку 
+
         }
     }
 }
---
+++ Управляющая компания ЖКХ/Услуги.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Управляющая_компания_ЖКХ
+{
+    /// <summary>
+    /// Логика взаимодействия для Услуги.xaml
+    /// </summary>
+    public partial class Услуги : Page
+    {
+        public Услуги()
+        {
+            InitializeComponent();
+        }
+
+        private void DobZ_Click(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection("server=ngknn.ru;Trusted_Connection=No;DataBase=43П_ЖКХ;User ID=33П;Password=12357"))
+            {
+                sqlConnection.Open();
+                IdS = ID_Services.Text;
+                IdSt = ID_Staff.Text;
+                countServ = CountServ.Text;
+                comm = Comm.Text;
+                dateServ = DateServ.Text;
+                int id1 = Convert.ToInt32(IdS);
+                int id2 = Convert.ToInt32(IdI);
+                DataTable sqlCommand = Select($"UPDATE [dbo].[Услуги] SET [ID_Staff] = '{id2}',[count] = '{countServ}' ,[Text] = '{comm}' ,[date] = '{dateServ}' WHERE [ID_Services] = '{id1}';");
+                sqlConnection.Close();
+            }
+            Close();
+        }
+    }
+}
